e5056d6af45cb540d5a27e7f3933b92a0930d33d
Illamapalooza <juliusbaliling25@gmail.com>
2023-11-17 15:22:11 +0800

    modified database to handle only COR information

--- cs_database.sql
+++ cs_database.sql
@@ -9,26 +9,18 @@ CREATE TABLE `building` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `department` (
+CREATE TABLE `faculty` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
-  `college` varchar(255) DEFAULT NULL,
-  `campus` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `faculty` (
-  `id` int NOT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `room` (
-  `id` varchar(10) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) DEFAULT NULL,
+  `room_number` int DEFAULT NULL,
+  `floor_no` int DEFAULT NULL,
   `building_id` int DEFAULT NULL,
-  `floor` int DEFAULT NULL,
-  `number` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_RoomBuilding` (`building_id`),
   CONSTRAINT `FK_RoomBuilding` FOREIGN KEY (`building_id`) REFERENCES `building` (`id`)
@@ -36,63 +28,44 @@ CREATE TABLE `room` (
 
 CREATE TABLE `schedule` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `semester_id` int DEFAULT NULL,
   `subject_id` int DEFAULT NULL,
-  `section_id` varchar(10) DEFAULT NULL,
   `faculty_id` int DEFAULT NULL,
-  `room_id` varchar(10) DEFAULT NULL,
-  `day` varchar(20) DEFAULT NULL,
+  `room_id` int DEFAULT NULL,
+  `section` varchar(10) DEFAULT NULL,
   `start_time` time DEFAULT NULL,
   `end_time` time DEFAULT NULL,
+  `weekday` varchar(20) DEFAULT NULL,
+  `semester` int DEFAULT NULL,
+  `year` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_ScheduleSemester` (`semester_id`),
-  KEY `FK_ScheduleFaculty` (`faculty_id`),
-  KEY `FK_ScheduleRoom` (`room_id`),
-  KEY `FK_ScheduleSection` (`section_id`),
+  KEY `FK_ScheduleSubject_idx` (`subject_id`),
+  KEY `FK_ScheduleRoom_idx` (`room_id`),
+  KEY `FK_ScheduleFaculty_idx` (`faculty_id`),
   CONSTRAINT `FK_ScheduleFaculty` FOREIGN KEY (`faculty_id`) REFERENCES `faculty` (`id`),
   CONSTRAINT `FK_ScheduleRoom` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`),
-  CONSTRAINT `FK_ScheduleSection` FOREIGN KEY (`section_id`) REFERENCES `section` (`id`),
-  CONSTRAINT `FK_ScheduleSemester` FOREIGN KEY (`semester_id`) REFERENCES `semester` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `section` (
-  `id` varchar(10) NOT NULL,
-  `department_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_SectionDepartment` (`department_id`),
-  CONSTRAINT `FK_SectionDepartment` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `semester` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `year` int DEFAULT NULL,
-  `period` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `FK_ScheduleSubject` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `student` (
   `id` int NOT NULL,
-  `track_id` int DEFAULT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
-  `nationality` varchar(255) DEFAULT NULL,
-  `birthday` date DEFAULT NULL,
-  `Gender` enum('Male','Female') DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL,
-  `year_level` int DEFAULT NULL,
   `age` int DEFAULT NULL,
-  `section_id` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_studentTrack` (`track_id`),
-  KEY `FK_studentSection_idx` (`section_id`),
-  CONSTRAINT `FK_studentSection` FOREIGN KEY (`section_id`) REFERENCES `section` (`id`),
-  CONSTRAINT `FK_studentTrack` FOREIGN KEY (`track_id`) REFERENCES `track` (`id`)
+  `gender` enum('Male','Female') DEFAULT NULL,
+  `year_level` int DEFAULT NULL,
+  `nationality` varchar(255) DEFAULT NULL,
+  `department` varchar(255) DEFAULT NULL,
+  `college` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `contact_no` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `student_schedule` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `student_id` int DEFAULT NULL,
   `schedule_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `FK_StudentScheduleProfile` (`student_id`),
   KEY `FK_ScheduleStudentProfile` (`schedule_id`),
   CONSTRAINT `FK_ScheduleStudentProfile` FOREIGN KEY (`schedule_id`) REFERENCES `schedule` (`id`),
@@ -100,22 +73,14 @@ CREATE TABLE `student_schedule` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `subject` (
-  `course_code` varchar(45) NOT NULL,
-  `description` text,
-  `lecture_units` int DEFAULT NULL,
-  `laboratory_units` int DEFAULT NULL,
-  `credit_units` int DEFAULT NULL,
-  PRIMARY KEY (`course_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
-CREATE TABLE `track` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `FKtrack_id` FOREIGN KEY (`id`) REFERENCES `student` (`track_id`)
+  `course_code` varchar(45) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `lecture_units` int DEFAULT NULL,
+  `lab_units` int DEFAULT NULL,
+  `credit_units` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 
 
 
